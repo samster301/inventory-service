e1fb6b202dfb1d61b78035b576a8b0b55bb88270
kokeb Beyene <kokzbest@gmail.com>
2019-09-25 22:59:04 -0500

    inventory service commit

---
+++ inventory-master/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO INVENTORY(inventory_Id,product_Id,qty,pick_Zone,pack_Zone,Shipping_Area)
+VALUES
+    (10002,30,8,'Ranga','E1234567','E1234567'),
+    (10003,30,8,'Ranga','E1234567','E1234567'),
+    (10004,30,8,'Ranga','E1234567','E1234567');
+    
